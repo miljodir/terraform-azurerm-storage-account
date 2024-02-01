483e1eec5f7c27611a73386c672dfc7c6b437a84
Erlend M <ermab95@gmail.com>
2024-02-01 12:01:43 +0100

    adds private endpoints to output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "storage_account" {
   description = "Output from all values for the created storage account."
   value       = azurerm_storage_account.account
 }
+
+output "private_endpoints" {
+  value       = azurerm_private_endpoint.pe
+  description = "Outputs private endpoints created for the storage account."
+}
