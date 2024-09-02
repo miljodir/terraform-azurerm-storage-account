d87014ea3c31608e741729a224b2bb8d2a93e07a
audunsolemdal <audun@solom.no>
2024-09-02 16:01:02 +0200

    Ensure terraform uses azurerm 4.0 provider

--- providers.tf
+++ providers.tf
@@ -3,12 +3,12 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "~> 3.0, < 5.0"
+      version               = "> 3.0, < 5.0"
       configuration_aliases = [azurerm.p-dns]
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5"
+      version = "> 3.0, < 5.0"
     }
   }
 }
